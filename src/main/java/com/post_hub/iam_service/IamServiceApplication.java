23a2a7a80ddd80396540675ab6e3c3d00c99e0e2
guzleek <guzl1k.hg@gmail.com>
2026-04-21 23:39:24 +0300

    create first request

--- src/main/java/com/post_hub/iam_service/IamServiceApplication.java
+++ src/main/java/com/post_hub/iam_service/IamServiceApplication.java
@@ -2,12 +2,25 @@ package com.post_hub.iam_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.flywaydb.core.Flyway;
 
 @SpringBootApplication
 public class IamServiceApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(IamServiceApplication.class, args);
-	}
+    public static void main(String[] args) {
 
+        Flyway flyway = Flyway.configure()
+                .dataSource(
+                        "jdbc:postgresql://localhost:5432/post_hub_local",
+                        "postgres",
+                        "secret123"
+                )
+                .schemas("v1_iam_service")
+                .locations("classpath:db/migration")
+                .load();
+
+        flyway.migrate(); // ручной запуск миграций
+
+        SpringApplication.run(IamServiceApplication.class, args);
+    }
 }
