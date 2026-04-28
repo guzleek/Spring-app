25549bf17be50a4c9e7326585fc3b5ba5613ed48
guzleek <guzl1k.hg@gmail.com>
2026-04-28 22:39:58 +0300

    PostController, add Get request

--- src/main/java/com/post_hub/iam_service/mapper/PostMapper.java
+++ src/main/java/com/post_hub/iam_service/mapper/PostMapper.java
@@ -2,6 +2,7 @@ package com.post_hub.iam_service.mapper;
 
 import com.post_hub.iam_service.model.dto.post.PostDTO;
 import com.post_hub.iam_service.model.entity.Post;
+import com.post_hub.iam_service.model.request.post.PostRequest;
 import org.hibernate.type.descriptor.DateTimeUtils;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
@@ -18,4 +19,8 @@ public interface PostMapper {
     @Mapping(source = "likes", target = "likes")
     @Mapping(source = "created", target = "created", dateFormat = "yyyy-MM-dd'T'HH:mm:ss")
     PostDTO toPostDto(Post post);
+
+    @Mapping(target = "id", ignore = true)
+    @Mapping(target = "created", ignore = true)
+    Post createPost(PostRequest postRequest);
 }
