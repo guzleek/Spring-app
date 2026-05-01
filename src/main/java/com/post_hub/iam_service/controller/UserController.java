770c7b43134e00b3ac60cc17a7e27c5d9eb5d025
guzleek <guzl1k.hg@gmail.com>
2026-05-01 22:00:20 +0300

    Add Create user

--- src/main/java/com/post_hub/iam_service/controller/UserController.java
+++ src/main/java/com/post_hub/iam_service/controller/UserController.java
@@ -3,17 +3,16 @@ package com.post_hub.iam_service.controller;
 import com.post_hub.iam_service.model.constants.ApiErrorMessage;
 import com.post_hub.iam_service.model.constants.ApiLogMessage;
 import com.post_hub.iam_service.model.dto.user.UserDTO;
+import com.post_hub.iam_service.model.request.user.NewUserRequest;
 import com.post_hub.iam_service.model.response.IamResponse;
 import com.post_hub.iam_service.service.UserService;
 import com.post_hub.iam_service.utils.ApiUtils;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @Slf4j
 @RestController
@@ -31,4 +30,14 @@ public class UserController {
         IamResponse<UserDTO> response = userService.getById(userId);
         return ResponseEntity.ok(response);
     }
+
+    @PostMapping("/create")
+    public ResponseEntity<IamResponse<UserDTO>> createUser (
+            @RequestBody @Valid NewUserRequest request) {
+        log.trace(ApiLogMessage.NAME_OF_CURRENT_METHOD.getValue(), ApiUtils.getMethodName());
+
+        IamResponse<UserDTO> response = userService.createUser(request);
+        return ResponseEntity.ok(response);
+    }
+
 }
--- src/main/java/com/post_hub/iam_service/model/entity/User.java
+++ src/main/java/com/post_hub/iam_service/model/entity/User.java
@@ -32,10 +32,10 @@ public class User {
     private String email;
 
     @Column(nullable = false)
-    private LocalDateTime created;
+    private LocalDateTime created = LocalDateTime.now();
 
     @Column(nullable = false)
-    private LocalDateTime updated;
+    private LocalDateTime updated = LocalDateTime.now();
 
     @Column(nullable = false)
     private LocalDateTime last_login;
